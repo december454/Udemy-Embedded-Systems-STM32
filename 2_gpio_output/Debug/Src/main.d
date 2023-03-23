b9a4f5d9c18383173c674d5a7c1e03f1b77c0da6
Griffin White <52751516+december454@users.noreply.github.com>
2023-03-23 14:19:57 -0500

    Completed "2_gpio_output"
    * This code follows along with Section 2: Video 11.
     * Like the prior projects, this program will blink the blue LED, "LD2".
     * This program provides utilizes pre-made libraries. This simplifies our code, removing the need to manually define memory addresses and offsets.

--- 2_gpio_output/Debug/Src/main.d
+++ 2_gpio_output/Debug/Src/main.d
@@ -1,3 +1,17 @@
 Src/main.o: ../Src/main.c \
- C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+ C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f412zx.h \
+ C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
 C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f412zx.h:
+C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/Griffin\ White/STM32CubeIDE/STM32Embedded-C_SourceCode/STM32Embedded-C_SourceCode/CubeIDE-Gpio_BareMetal/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
--- 2_gpio_output/Src/main.c
+++ 2_gpio_output/Src/main.c
@@ -1,15 +1,29 @@
-#include "stm32f4xx.h"
+// Griffin White
+// 3-23-2023
+// 2 - GPIO Output
+// Udemy - Embedded Systems Bare-Metal Programming Ground Up (STM32)
 
+/* This code follows along with Section 2: Video 11.
+ * Like the prior projects, this program will blink the blue LED, "LD2".
+ * This program provides utilizes pre-made libraries. This simplifies our code, removing the need
+ * to manually define memory addresses and offsets.
+ */
+
+#include "stm32f4xx.h"
 
 /////////////////////////////////// Additional Constants ///////////////////////////////////
 #define GPIOBEN				(1U<<1)
 #define BLUE_LED_PIN		(1U<<7)
 /////////////////////////////////// Additional Constants End ///////////////////////////////
 
-
 int main (void){
 
-	while(1){
+	RCC->AHB1ENR |= GPIOBEN;
+	GPIOB->MODER |= (1U<<14);
+	GPIOB->MODER &=~ (1U<<15);
 
+	while(1){
+		GPIOB->ODR ^= BLUE_LED_PIN;
+		for (int i = 0; i < 200000; i++){}
 	}
 }
